f29ea8337583f2064b9116b357dd6d1735720001
Tomás Pietravallo <hello@tomaspietravallo.com>
2024-11-17 15:22:32 -0300

    chore: embed as iframe, add csp
    chore: update csp

---
+++ next.config.mjs
@@ -0,0 +1,31 @@
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+	frame-src 'self' https://tomaspietravallo.com;
+    frame-ancestors https://tomaspietravallo.com;
+`
+
+/** @type {import('next').NextConfig} */
+export default {
+    pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
+	async headers() {
+		return [
+		{
+			source: "/(.*)",
+			headers: [
+			{ key: 'Content-Security-Policy', value: cspHeader.replace(/\n/g, '') },
+			{ key: "X-Frame-Options", value: "SAMEORIGIN" },
+			]
+		}
+		];
+	},
+	devIndicators: {
+		appIsrStatus: false
+	}
+}
